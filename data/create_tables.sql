5d16dd963ac57001079cc609bdfe0221edca1897
JM-Laffargue <laffargue.jm@gmail.com>
2023-02-16 16:42:37 +0100

    creation du fichier create_table.sql

---
+++ data/create_tables.sql
@@ -0,0 +1,57 @@
+BEGIN;
+
+DROP TABLE IF EXISTS "role",
+"user",
+"point",
+"student",
+"house";
+
+CREATE TABLE IF NOT EXISTS "role" (
+  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "name" TEXT NOT NULL UNIQUE,
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMPZ
+);
+
+CREATE TABLE IF NOT EXISTS "user" (
+  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "lastname" TEXT NOT NULL,
+  "firstname" TEXT NOT NULL,
+  "email" TEXT NOT NULL UNIQUE,
+  "password" TEXT NOT NULL,
+  "role_id" INT NOT NULL REFERENCES "role"("id"),
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMPZ
+);
+
+CREATE TABLE IF NOT EXISTS "house" (
+  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "name" TEXT NOT NULL UNIQUE,
+  "score" INT,
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMPZ
+);
+
+CREATE TABLE IF NOT EXISTS "student" (
+  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "lastname" TEXT NOT NULL,
+  "firstname" TEXT NOT NULL,
+  "class" TEXT NOT NULL,
+  "score" INT,
+  "house_id" INT NOT NULL REFERENCES "house"("id"),
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMPZ
+);
+
+CREATE TABLE IF NOT EXISTS "point" (
+  "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "value" TEXT NOT NULL,
+  "content" TEXT NOT NULL UNIQUE,
+  "user_id" INT NOT NULL REFERENCES "user"("id"),
+  "house_id" INT NOT NULL REFERENCES "house"("id"),
+  "role_id" INT NOT NULL REFERENCES "role"("id"),
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMPZ
+);
+
+COMMIT;
