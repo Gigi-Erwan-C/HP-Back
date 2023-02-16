60a4b07c8c072f4652a8f6eb2f5354409b413e80
Gigi <gigi46.hd48@gmail.com>
2023-02-16 13:36:53 +0100

    Fix eslint

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ const app = express();
 // app.use(router);
 
 app.listen(port, () => {
-  console.log(`Server ready: http://localhost:${port}`);
+    console.log(`Server ready: http://localhost:${port}`);
 });
