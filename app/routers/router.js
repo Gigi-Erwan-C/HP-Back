76f0e844ba4b6c9b4e91d753d1791affef5a04dc
JM-Laffargue <laffargue.jm@gmail.com>
2023-02-21 14:35:55 +0100

    fix conflicts on router.js

--- app/routers/router.js
+++ app/routers/router.js
@@ -4,11 +4,8 @@ const router = express.Router();
 
 const userController = require('../controllers/userController');
 const roleController = require('../controllers/roleController');
-<<<<<<< HEAD
 const studentController = require('../controllers/studentController');
-=======
 const houseController = require('../controllers/houseController');
->>>>>>> method-get-house-id
 
 router.get('/admin/user', userController.getAll);
 router.post('/admin/user', userController.add);
@@ -17,11 +14,9 @@ router.get('/admin/user/:id', userController.getOne);
 router.patch('/admin/user/:id', userController.update);
 router.delete('/admin/user/:id', userController.delete);
 
-<<<<<<< HEAD
 router.get('/admin/student', studentController.getAll);
-=======
+
 router.get('/house/:id', houseController.getOne);
->>>>>>> method-get-house-id
 
 router.get('/role', roleController.getRole);
 
