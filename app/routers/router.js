26fbbc5bbf6ea0200ca1ce3fd05a879e1af49e0e
Gigi <gigi46.hd48@gmail.com>
2023-02-21 16:06:31 +0100

    Rename branch

--- app/routers/router.js
+++ app/routers/router.js
@@ -5,14 +5,22 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const roleController = require('../controllers/roleController');
 const studentController = require('../controllers/studentController');
+const houseController = require('../controllers/houseController');
 
 router.get('/admin/user', userController.getAll);
 router.post('/admin/user', userController.add);
 
 router.get('/admin/user/:id', userController.getOne);
 router.patch('/admin/user/:id', userController.update);
+router.delete('/admin/user/:id', userController.delete);
 
-router.get('/admin/student', studentController.getAll);
+router.get('/student', studentController.getAll);
+router.post('/admin/student', studentController.addStudent);
+
+router.get('/house', houseController.getAll);
+router.get('/house/:id', houseController.getOne);
+router.get('/house/:id/student', houseController.getAllStudentsFromOneHouse);
+router.patch('/admin/house/:id', houseController.update);
 
 router.get('/role', roleController.getRole);
 
