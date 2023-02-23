67290bc8c7cee020656d960c49cc526304172dd5
JM-Laffargue <laffargue.jm@gmail.com>
2023-02-23 10:30:04 +0100

    feat error handler and try/catch on controller

--- app/controllers/pointController.js
+++ app/controllers/pointController.js
@@ -6,11 +6,21 @@ const pointController = {
     res.json(result);
   },
 
+  // eslint-disable-next-line consistent-return
   async add(req, res) {
-    const pointInfo = req.body;
+    try {
+      const pointInfo = req.body;
 
-    const result = await dataMapper.addPoint(pointInfo);
-    res.json(result);
+      if ((!pointInfo.house_id && !pointInfo.student_id)
+      || (pointInfo.house_id && pointInfo.student_id)) {
+        return res.status(400).json({ error: 'Données incorrectes, merci de rééssayer' });
+      }
+
+      const result = await dataMapper.addPoint(pointInfo);
+      res.json(result);
+    } catch (error) {
+      res.status(500).json({ error: 'Requête invalide' });
+    }
   },
 
   async getOne(req, res) {
