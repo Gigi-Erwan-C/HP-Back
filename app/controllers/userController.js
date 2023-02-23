c9a67ebdd5dc047d4d7f90be0c8468051ae61920
Gigi <gigi46.hd48@gmail.com>
2023-02-23 17:22:49 +0100

    feat: add bcrypt and email-validator in userController method update, test insomnia ok

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -49,8 +49,34 @@ const userController = {
 
   async update(req, res) {
     const { id } = req.params;
-    const userInfo = req.body;
-    const result = await dataMapper.updateUser({ ...userInfo, id });
+    const {
+      // eslint-disable-next-line camelcase
+      lastname, firstname, email, password, role_id,
+    } = req.body;
+
+    if (!emailValidator.validate(email)) {
+      res.status(400).send({ message: 'L\'email renseigné est dans un format invalide.' });
+      return;
+    }
+
+    if (!password || password.length < 4) {
+      res.status(400).send({ message: 'Le mot de passe doit faire plus de 4 caractères.' });
+      return;
+    }
+
+    const saltRounds = parseInt(process.env.SALT_ROUNDS, 10);
+
+    const encryptedPassword = await bcrypt.hash(password, saltRounds);
+
+    const result = await dataMapper.updateUser({
+      lastname,
+      firstname,
+      email,
+      password: encryptedPassword,
+      // eslint-disable-next-line camelcase
+      role_id,
+      id,
+    });
     res.json(result);
   },
 
