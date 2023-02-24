a81433e535d4b3637535eb75f7a367d35866211d
JM-Laffargue <laffargue.jm@gmail.com>
2023-02-24 15:32:50 +0100

    feat CORS

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 
 const router = require('./routers/router');
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use(router);
 
