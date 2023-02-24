3a548d7345d4cbcb8a0539a233e8227bb659fe3e
JM-Laffargue <laffargue.jm@gmail.com>
2023-02-24 14:04:48 +0100

    add connectionString for railway to database.js

--- app/models/database.js
+++ app/models/database.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
 
 module.exports = pool;
