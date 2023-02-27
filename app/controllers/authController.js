74af52523653d7c160987c3df836f9a02a971eee
Gigi <gigi46.hd48@gmail.com>
2023-02-27 10:50:15 +0100

    fix: add bcrypt compare to password authController, add getOneUserByEmail in datamapper, test insomnia ok

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,14 +1,23 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const dataMapper = require('../models/dataMapper');
 
 const authController = {
   // eslint-disable-next-line consistent-return
   async login(req, res) {
-    const userInfo = req.body;
-    const result = await dataMapper.checkUserInfo(userInfo);
+    const { email, password } = req.body;
+    console.log(email);
+    const result = await dataMapper.getOneUserByEmail(email);
+    console.log(result);
     if (!result) {
-      return res.status(401).json({ message: 'E-mail ou mot de passe incorrect' });
+      return res.status(401).json({ message: 'EE-mail ou mot de passe incorrect' });
     }
+    const isMatchingPassword = await bcrypt.compare(password, result.password);
+
+    if (!isMatchingPassword) {
+      return res.status(401).send({ errorMessage: 'E-mail ou mot de passe incorrect' });
+    }
+
     // eslint-disable-next-line max-len
     result.token = jwt.sign(result, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.status(200).json({ result });
