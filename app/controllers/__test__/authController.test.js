1d01c87a71f20aa6f37f806194b8a42069b74bbe
JM-Laffargue <laffargue.jm@gmail.com>
2023-03-08 14:17:21 +0100

    test test test

--- app/controllers/__test__/authController.test.js
+++ app/controllers/__test__/authController.test.js
@@ -1,11 +1,11 @@
 const authController = require('../authController');
 
 describe('Tests du login', () => {
-  it('should return a 401 status with an error if there is no result', async () => {
+  it('should return a 401 status with an error message if there is no result', async () => {
     const mReq = { body: { email: '', password: '' } };
     const mRes = { status: jest.fn(), json: jest.fn().mockReturnValue({ message: 'E-mail ou mot de passe incorrecte' }) };
     await authController.login(mReq, mRes);
-    expect(mRes.status).toBeCalledWith(401);
-    expect(mRes.json).toBeCalledWith({ message: 'E-mail ou mot de passe incorrecte' });
+    (mRes.status).toBeCalledWith(401);
+    (mRes.json).toBeCalledWith({ message: 'E-mail ou mot de passe incorrecte' });
   });
 });
