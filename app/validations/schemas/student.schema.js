88b4fe9c890156a9d8a69cf3e101f3f7efdcebc6
JM-Laffargue <laffargue.jm@gmail.com>
2023-03-09 12:03:19 +0100

    fix eslint error on schemas

--- app/validations/schemas/student.schema.js
+++ app/validations/schemas/student.schema.js
@@ -1,20 +1,20 @@
 const Joi = require('joi');
 
 const schemas = {
-    post: Joi.object({
-        lastname: Joi.string().required(),
-        firstname: Joi.string().required(),
-        class_name: Joi.string(),
-        score: Joi.number().integer(),
-        house_id: Joi.number().integer(),
-    }).required(),
-    patch: Joi.object({
-        lastname: Joi.string().required(),
-        firstname: Joi.string().required(),
-        class_name: Joi.string(),
-        score: Joi.number().integer(),
-        house_id: Joi.number().integer(),
-    }).required(),
+  post: Joi.object({
+    lastname: Joi.string().required(),
+    firstname: Joi.string().required(),
+    class_name: Joi.string(),
+    score: Joi.number().integer(),
+    house_id: Joi.number().integer(),
+  }).required(),
+  patch: Joi.object({
+    lastname: Joi.string().required(),
+    firstname: Joi.string().required(),
+    class_name: Joi.string(),
+    score: Joi.number().integer(),
+    house_id: Joi.number().integer(),
+  }).required(),
 };
 
 module.exports = schemas;
--- app/validations/schemas/user.schema.js
+++ app/validations/schemas/user.schema.js
@@ -1,27 +1,27 @@
 const Joi = require('joi');
 
 const schemas = {
-    post: Joi.object({
-        lastname: Joi.string().required(),
-        firstname: Joi.string().required(),
-        email: Joi.string().pattern(/^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/).required(),
-        password: Joi.string().required(),
-        role_id: Joi.number().integer().required(),
-    }).required(),
-    patch: Joi.object({
-        lastname: Joi.string().required(),
-        firstname: Joi.string().required(),
-        email: Joi.string().pattern(/^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/).required(),
-        role_id: Joi.number().integer().required(),
-    }).required(),
-    patchPasswordAdmin: Joi.object({
-        password: Joi.string().required(),
-    }).required(),
-    patchPasswordUser: Joi.object({
-        oldPassword: Joi.string().required(),
-        password: Joi.string().required(),
-        confirmation: Joi.string().required(),
-    }).required(),
+  post: Joi.object({
+    lastname: Joi.string().required(),
+    firstname: Joi.string().required(),
+    email: Joi.string().pattern(/^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/).required(),
+    password: Joi.string().required(),
+    role_id: Joi.number().integer().required(),
+  }).required(),
+  patch: Joi.object({
+    lastname: Joi.string().required(),
+    firstname: Joi.string().required(),
+    email: Joi.string().pattern(/^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/).required(),
+    role_id: Joi.number().integer().required(),
+  }).required(),
+  patchPasswordAdmin: Joi.object({
+    password: Joi.string().required(),
+  }).required(),
+  patchPasswordUser: Joi.object({
+    oldPassword: Joi.string().required(),
+    password: Joi.string().required(),
+    confirmation: Joi.string().required(),
+  }).required(),
 };
 
 module.exports = schemas;
