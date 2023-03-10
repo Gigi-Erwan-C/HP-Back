7aa403356a684828cf59d219caad66adb7c55088
JM-Laffargue <laffargue.jm@gmail.com>
2023-03-10 09:38:49 +0100

    fix validate.js

--- app/validations/validate.js
+++ app/validations/validate.js
@@ -1,13 +1,13 @@
 function validate(schema, dataSource) {
-    return async (request, response, next) => {
-      try {
-        await schema.validateAsync(request[dataSource]);
-        next();
+  return async (req, res, next) => {
+    try {
+      await schema.validateAsync(req[dataSource]);
+      next();
     } catch (error) {
-        console.error(error);
-        res.status(400).json({ error: "Bad input error. Please try again later." });
-      }
-    };
-  }
-  
-  module.exports = validate;
+      console.error(error);
+      res.status(400).json({ error: 'Bad input error. Please try again later.' });
+    }
+  };
+}
+
+module.exports = validate;
